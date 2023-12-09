ebdaf08b5c090a336a070f2fa7d97a63ee96b257
Vasjeni <vasilyev508@gmail.com>
2023-12-09 18:11:06 +0000

    added display of the second model o the homepage.html

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,14 @@
 from django.views.generic import TemplateView, ListView
 from services.models import Services
+from feedback.models import Feedback
 from django.utils.translation import get_language_from_request
 
 # Create your views here.
 
 
 class HomePageView(ListView):
-    model = Services
-    context_object_name = "service_list"
+    # model = Services
+    # context_object_name = "service_list"
     template_name = "home.html"
 
     def get_queryset(self):
@@ -16,12 +17,11 @@ class HomePageView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context.update(
-            {
-                "lang": get_language_from_request(self.request),
-                # "queryset": super().get_queryset(),
-            }
-        )
+        services = Services.objects.all()
+        feedback = Feedback.objects.all()
+        context["services"] = services
+        context["feedbacks"] = feedback
+        context["lang"] = get_language_from_request(self.request)
         return context
 
 
